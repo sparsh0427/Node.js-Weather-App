659a7ab0ccbcb3bdee6e9dd96edc1410063503bc
sparsh0427 <sparsh.chadha27@gmail.com>
2020-07-03 01:53:08 +0530

    Fixed errors

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.get('', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-    res.render('About', {
+    res.render('about', {
         title: 'About me',
         name: 'Sparsh'
     })
